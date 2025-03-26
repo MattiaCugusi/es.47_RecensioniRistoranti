1efcccce580c88fd5167d11e8422d457e95cb844
Mattia Cugusi <m.cugusi287gmail.com>
2025-03-26 15:53:29 +0100

    parte 1 progetto completata

--- errore_loginreg.php
+++ errore_loginreg.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
 <h2 style='color: red'>Ops.. qualcosa e' andato storto</h2>
@@ -16,7 +17,8 @@ session_start();
     $_SESSION["errore"] = "";
     ?>
 
-    <a href="paginalogin.html">Torna al login</a>
+
+    <a href="paginalogin.html"><i class="bi bi-skip-backward-fill"></i> Torna al login</a>
 
 </body>
 </html>
