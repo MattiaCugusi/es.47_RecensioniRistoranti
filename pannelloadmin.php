3ff7f295ce2449ca7ddaa297222a3a5cf257f75e
= <m.cugusi287@gmail.com>
2025-06-05 09:38:52 +0200

    miglioramenti grafici

--- pannelloadmin.php
+++ pannelloadmin.php
@@ -11,9 +11,8 @@ session_start();
     <title>Pannello admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="crossorigin=""/>
+    <link rel="stylesheet" href="./css/stylesAdmin.css">
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
@@ -38,7 +37,9 @@ session_start();
       if($c->num_rows > 0){
         if ($r->num_rows > 0){
         
-        echo "<table class='table' style='text-align: center'> <thead>";
+      
+        echo "<div style = 'width: 100%'>";
+        echo "<table class='table' style='text-align: center' width:'50%'> <thead>";
        
         echo "<tr>";
         foreach($c as $colonne){
@@ -61,34 +62,32 @@ session_start();
         }
         echo "</tbody>";
         echo "</table>";
+        echo "</div>";
 
         echo "<br>
-        <form style='text-align:center' action='inserisciristorante.php' method='post'>
-        <h1>Inserisci nuovo ristorante:</h1>
-        <br>
+       <div class='form-container'  style = 'width: 70%'; text-align: center>>
+    <form action='inserisciristorante.php' method='post'>
+        <h1>Inserisci nuovo ristorante</h1>
+        
         <label>Nome Ristorante:</label>
-        <br>
-        <input type='text' name='nome'>
-        <br>
+        <input type='text' name='nome' required>
+        
         <label>Indirizzo:</label>
-        <br>
-        <input type='text' name='indirizzo'>
-        <br>
-        <label>Citta':</label>
-        <br>
-        <input type='text' name='citta'>
-        <br>
+        <input type='text' name='indirizzo' required>
+        
+        <label>Città:</label>
+        <input type='text' name='citta' required>
+        
         <label>Latitudine:</label>
-        <br>
-        <input type='text' value='43.7800127' name='latitudine'>
-        <br>
+        <input type='text' name='latitudine' value='43.7800127'>
+        
         <label>Longitudine:</label>
-        <br>
-        <input type='text' value='11.1997685' name='longitudine'>
-        <br>
-        <br>
+        <input type='text' name='longitudine' value='11.1997685'>
+        
         <button type='submit'>Aggiungi</button>
-        </form>";
+    </form>
+</div>";
+
 
         if(isset($_SESSION["errore"])){
            echo "<p style = 'color: red; background-color: black; text-align: center'>" . $_SESSION["errore"] . "</p>"; 
